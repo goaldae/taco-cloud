4a6a4d68a6fb72dcc96105141a9dd9f72169eb20
goaldae <goaldae@naver.com>
2020-12-15 17:03:36 +0900

    Refactor controller for model and add order, taco repository

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,12 +1,18 @@
 package tacos;
 
 import lombok.Data;
+import java.util.Date;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class Order {
+	private Long id;
+	private Date placedAt;
 	
 	@NotBlank(message="Name is required")
 	private String deliveryName;
@@ -32,4 +38,10 @@ public class Order {
 	
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+	
+	private List<Taco> tacos = new ArrayList<>();
+	
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
 }
