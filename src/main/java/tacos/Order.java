7bb902a9c29df194188c3173e8c8eb5126e6e503
goaldae <goaldae@naver.com>
2021-02-22 14:43:19 +0900

    Change jdbc to JPA, add security

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,13 +1,24 @@
 package tacos;
 
-import lombok.Data;
-import java.util.Date;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.CreditCardNumber;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import lombok.Data;
 
 @Data
 @Entity
@@ -45,9 +56,15 @@ public class Order implements Serializable{
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
 	
+	@ManyToMany(targetEntity=Taco.class)
 	private List<Taco> tacos = new ArrayList<>();
 	
 	public void addDesign(Taco design) {
 		this.tacos.add(design);
+	}
+	
+	@PrePersist
+	void placedAt() {
+		this.placedAt = new Date();
 	}
 }
